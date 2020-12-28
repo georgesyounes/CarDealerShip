02b5cabc3371be3d914cb9d6a03537a5883f844a
unknown <georgesyounes>
2020-12-28 22:42:58 +0200

    scd

--- connection/connection.php
+++ connection/connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; 
 $username = "root"; 
 $password = null; 
-$dbname = "mydb"; 
+$dbname = "blog_samples"; 
   
 // checking connection 
 $connection = new mysqli($servername, $username, $password, $dbname); 
